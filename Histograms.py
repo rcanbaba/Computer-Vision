c2c812e076085a06057bcebde9d63aea4a328475
rcanbaba <32519328+rcanbaba@users.noreply.github.com>
2018-10-20 20:52:09 +0300

    Added view
    Input and target images histograms are achieved. 
    Also view designed.

--- Histograms.py
+++ Histograms.py
@@ -1,202 +1,192 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 00:14:00 2018
+
+@author: anildogan
+"""
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
-    QPushButton, QGroupBox, QAction, QFileDialog
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from PyQt5.QtWidgets import QGridLayout
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import Qt
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-import numpy as np
+from PyQt5.QtWidgets import QMainWindow,QMessageBox, QApplication,QScrollArea, QWidget, QPushButton, QAction, QGroupBox, QFileDialog, QLabel, QVBoxLayout, QGridLayout, QHBoxLayout,QFrame, QSplitter,QSizePolicy
+from PyQt5.QtGui import QIcon, QPixmap, QPalette,QImage
+from PyQt5.QtCore import pyqtSlot, Qt
 import cv2
-#from PIL import Image
-
-
-##########################################
-## Do not forget to delete "return NotImplementedError"
-## while implementing a function
-########################################
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 class App(QMainWindow):
+    
     def __init__(self):
-        super(App, self).__init__()
-    #    return NotImplementedError
-
-        action= QAction("&Open Input",self)
-        action.triggered.connect(self.openInputImage)
-
-        action2 = QAction ("&Open Target",self)
-        action2.triggered.connect(self.openTargetImage)
-
-        action3 = QAction ("&Exit",self)
-        action3.triggered.connect(self.closeApp)
-
-        mainmenu = self.menuBar()
-        fileMenu = mainmenu.addMenu("&file")
-        fileMenu.addAction(action2)
-        fileMenu.addAction(action3)
-        fileMenu.addAction(action)
-
-        self.title = 'Histogram Equalization'
-        self.showFullScreen()
-        # You can define other things in here
-        self.initUI()
-
-    def openInputImage(self):
-        # This function is called when the user clicks File->Input Image.
-        label = QLabel(self)
-        filename = QFileDialog.getOpenFileName()
-        imagePath = filename[0]
-        img = cv2.imread(imagePath)
-        #plt.subplot(4, 1, 1)
-        #cv2.imshow('image',img)
-        #blue, green, red = cv2.split(img)
-        red = img[:,:,2]
-        green = img[:,:,1]
-        blue = img[:,:,0]
-         #cv2.imshow('green',green)
-        #cv2.imshow('blue',blue)
-        #cv2.imshow('red',red)
-        fig = plt.figure()
-        width, height = blue.shape
-        i =0
-        blueArray = [0]* 256
-        for w in range(0,width):
-            for h in range(0,height):
-                temp = blue[w][h]
-                blueArray[temp]+=1
-
-        plt.subplot(4, 1, 4)
-        plt.bar(range(256), blueArray, color="blue")
-
-        width, height = red.shape
-        i =0
-        redArray = [0]* 256
-        for w in range(0,width):
-            for h in range(0,height):
-                temp = red[w][h]
-                redArray[temp]+=1
-        plt.subplot(4, 1, 2)
-        plt.bar(range(256), redArray, color="red")
-
-        width, height = green.shape
-        i =0
-        greenArray = [0]* 256
-        for w in range(0,width):
-            for h in range(0,height):
-                temp = green[w][h]
-                greenArray[temp]+=1
-        plt.subplot(4, 1, 3)
-        plt.bar(range(256), greenArray, color="green")
-        plt.show()
-
-        return NotImplementedError
-
-
-    def openTargetImage(self):
-        # This function is called when the user clicks File->Target Image.
-        label = QLabel(self)
-        filename = QFileDialog.getOpenFileName()
-        imagePath = filename[0]
-        img2 = cv2.imread(imagePath)
-        red = img2[:,:,2]
-        green = img2[:,:,1]
-        blue = img2[:,:,0]
-
-        fig = plt.figure()
-        width, height = blue.shape
-        i =0
-        blueArray = [0]* 256
-        for w in range(0,width):
-            for h in range(0,height):
-                temp = blue[w][h]
-                blueArray[temp]+=1
-
-        plt.subplot(4, 1, 3)
-        plt.bar(range(256), blueArray, color="blue")
-
-        width, height = red.shape
-        i =0
-        redArray = [0]* 256
-        for w in range(0,width):
-            for h in range(0,height):
-                temp = red[w][h]
-                redArray[temp]+=1
-        plt.subplot(4, 1, 2)
-        plt.bar(range(256), redArray, color="red")
-
-        width, height = green.shape
-        i =0
-        greenArray = [0]* 256
-        for w in range(0,width):
-            for h in range(0,height):
-                temp = green[w][h]
-                greenArray[temp]+=1
-        plt.subplot(4, 1, 4)
-        plt.bar(range(256), greenArray, color="green")
-        plt.show()
-
-
-        return NotImplementedError
-
-    def closeApp(self):
-
-        return NotImplementedError
-
-    def initUI(self):
-        #return NotImplementedError
-        # Write GUI initialization code
-
+        super(App,self).__init__()
+        
+        self.window = QWidget(self)
+        self.setCentralWidget(self.window)
+    
+        self.inputBox = QGroupBox('Input')
+        inputLayout = QVBoxLayout()
+        self.inputBox.setLayout(inputLayout)
+        
+        self.targetBox = QGroupBox('Target')
+        targetLAyout = QVBoxLayout()
+        self.targetBox.setLayout(targetLAyout)
+        
+        self.resultBox = QGroupBox('Result')
+        resultLayout = QVBoxLayout()
+        self.resultBox.setLayout(resultLayout)
+        
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.inputBox, 0, 0)
+        self.layout.addWidget(self.targetBox, 0, 1)
+        self.layout.addWidget(self.resultBox, 0, 2)
+        
+        self.window.setLayout(self.layout)
+        
+        self.image = None
+        self.image2 = None
+        self.figure = Figure()
+        self.figure2 = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.qImg = None
+        self.qImg2 = None
+        self.pixmap01 = None
+        self.pixmap_image = None
+       
+        self.createActions()
+        self.createMenu()
+        
+        self.setWindowTitle("Histogram")
+        self.showMaximized()
         self.show()
+        
+        
+    def createActions(self):
+        self.open_inputAct = QAction(' &Open Input',self)
+        self.open_inputAct.triggered.connect(self.open_Input)
+        self.open_targetAct = QAction(' &Open Target', self)
+        self.open_targetAct.triggered.connect(self.open_Target)
+        self.exitAct = QAction(' &Exit', self)
+        self.exitAct.triggered.connect(self.exit)
+    
+    def createMenu(self):
+        self.mainMenu = self.menuBar()
+        self.fileMenu = self.mainMenu.addMenu('File')
+        self.fileMenu.addAction(self.open_inputAct)
+        self.fileMenu.addAction(self.open_targetAct)
+        self.fileMenu.addAction(self.exitAct)
+    
+    def createHistogram(self):
+        red = self.image[:,:,2]
+        green = self.image[:,:,1]
+        blue = self.image[:,:,0]
+        width,height = blue.shape
+        
+        blueArray = [0]*256
+        redArray = [0]*256
+        greenArray = [0]*256
+        
+        for w in range(0,width):
+            for h in range(0,height):
+                temp = blue[w][h]
+                blueArray[temp]+=1
+        for w in range(0,width):
+            for h in range(0,height):
+                temp = red[w][h]
+                redArray[temp]+=1        
+        for w in range(0,width):
+            for h in range(0,height):
+                temp = green[w][h]
+                greenArray[temp]+=1 
+        blueplot = self.figure.add_subplot(313)
+        redplot = self.figure.add_subplot(311)
+        greenplot = self.figure.add_subplot(312)
+        
+        blueplot.bar(range(256),blueArray,color = 'blue')
+        redplot.bar(range(256),redArray,color = 'red')
+        greenplot.bar(range(256),greenArray,color = 'green')
+        self.canvas.draw()
+        self.inputBox.layout().addWidget(self.canvas)
+    def createHistogram2(self):
+        red = self.image2[:,:,2]
+        green = self.image2[:,:,1]
+        blue = self.image2[:,:,0]
+        width,height = blue.shape
+        
+        blueArray = [0]*256
+        redArray = [0]*256
+        greenArray = [0]*256
+        
+        for w in range(0,width):
+            for h in range(0,height):
+                temp = blue[w][h]
+                blueArray[temp]+=1
+        for w in range(0,width):
+            for h in range(0,height):
+                temp = red[w][h]
+                redArray[temp]+=1        
+        for w in range(0,width):
+            for h in range(0,height):
+                temp = green[w][h]
+                greenArray[temp]+=1 
+        blueplot = self.figure2.add_subplot(313)
+        redplot = self.figure2.add_subplot(311)
+        greenplot = self.figure2.add_subplot(312)
+        
+        blueplot.bar(range(256),blueArray,color = 'blue')
+        redplot.bar(range(256),redArray,color = 'red')
+        greenplot.bar(range(256),greenArray,color = 'green')
+        self.canvas2.draw()
+        self.targetBox.layout().addWidget(self.canvas2)
+    def open_Input(self):
+        #fileName, _ = QFileDialog.getOpenFileName(self, "Open File",QDir.currentPath())
+        fileName, _ = QFileDialog.getOpenFileName(self, 'Open Input', '.')
+        if fileName:
+            self.image = cv2.imread(fileName)
+            height,width,channels = self.image.shape
+            bytesPerLine = 3 * width
+            if not self.image.data:
+                QMessageBox.information(self, "Image Viewer",
+                        "Cannot load %s." % fileName)
+                return
+            
+        self.qImg = QImage(self.image.data,width,height,bytesPerLine,QImage.Format_RGB888).rgbSwapped()
+        #self.pixmap01 = QPixmap.fromImage(self.qImg)
+        
+        imageLabel = QLabel('image')
+        imageLabel.setPixmap(QPixmap.fromImage(self.qImg))
+        imageLabel.setAlignment(Qt.AlignCenter)
+        
+        self.inputBox.layout().addWidget(imageLabel)
+        self.createHistogram()   
+       # self.updateActions()
 
-    def histogramButtonClicked(self):
-        if not self.inputLoaded and not self.targetLoaded:
-            # Error: "First load input and target images" in MessageBox
-            return NotImplementedError
-        if not self.inputLoaded:
-            # Error: "Load input image" in MessageBox
-            return NotImplementedError
-        elif not self.targetLoaded:
-            # Error: "Load target image" in MessageBox
-            return NotImplementedError
-
-    def calcHistogram(self, I):
-        # Calculate histogram
-        return NotImplementedError
-
-    def createEmptyInputGroupBox(self):
-        self.inputGroupBox = QGroupBox('Input')
-        layout = QVBoxLayout()
-
-        self.inputGroupBox.setLayout(layout)
-
-    def createEmptyTargetGroupBox(self):
-        self.targetGroupBox = QGroupBox('Target')
-        layout = QVBoxLayout()
-
-        self.targetGroupBox.setLayout(layout)
-
-    def createEmptyResultGroupBox(self):
-        self.resultGroupBox = QGroupBox('Input')
-        layout = QVBoxLayout()
-
-        self.resultGroupBox.setLayout(layout)
-
-
-class PlotCanvas(FigureCanvas):
-    def __init__(self, hist, parent=None, width=5, height=4, dpi=100):
-        return NotImplementedError
-        # Init Canvas
-        self.plotHistogram(hist)
-
-    def plotHistogram(self, hist):
-        return NotImplementedError
-        # Plot histogram
-
-        self.draw()
-
-
+       
+    
+    def open_Target(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, 'Open Input', '.')
+        if fileName:
+            self.image2 = cv2.imread(fileName)
+            height,width,channels = self.image2.shape
+            bytesPerLine = 3 * width
+            if not self.image2.data:
+                QMessageBox.information(self, "Image Viewer",
+                        "Cannot load %s." % fileName)
+                return
+            
+        self.qImg2 = QImage(self.image2.data,width,height,bytesPerLine,QImage.Format_RGB888).rgbSwapped()
+        #self.pixmap01 = QPixmap.fromImage(self.qImg)
+        
+        imageLabel = QLabel('image')
+        imageLabel.setPixmap(QPixmap.fromImage(self.qImg2))
+        imageLabel.setAlignment(Qt.AlignCenter)
+        
+        self.targetBox.layout().addWidget(imageLabel)
+        self.createHistogram2()
+    
+    def exit(self):
+        sys.exit()
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
+    ex.show()
     sys.exit(app.exec_())
